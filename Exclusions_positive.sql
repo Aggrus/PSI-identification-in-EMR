2f0e57528807d41f7a7657b95a703fc052bc2612
Aggrus <gzalcman2015@my.fit.edu>
2018-06-24 13:43:17 +0300

    Whole lot of changes

--- Exclusions_positive.sql
+++ Exclusions_positive.sql
@@ -12,7 +12,7 @@ INNER JOIN diagnoses_icd  ON admissions.subject_id = diagnoses_icd.subject_id);
 --Checks that all information in the table conforms to the specifications
 SELECT admissions.subject_id, diagnoses_icd.icd9_code, PATIENTS.gender, admissions.admission_location, admissions.discharge_location 
 FROM admissions, patients, diagnoses_icd
-WHERE (diagnoses_icd.icd9_code IS NOT NULL) 
+WHERE (NOT diagnoses_icd.icd9_code = NULL) 
 AND (PATIENTS.gender = 'M' OR PATIENTS.gender = 'F') 
 AND (admissions.admission_location = 'TRANSFER FROM HOSP/EXTRAM' OR admissions.admission_location = 'EMERGENCY ROOM ADMIT' OR admissions.admission_location = 'PHYS REFERRAL/NORMAL DELI' OR admissions.admission_location = 'CLINIC REFERRAL/PREMATURE') 
 AND (admissions.discharge_location = 'DEAD/EXPIRED' OR admissions.discharge_location = 'HOME' OR admissions.discharge_location = 'HOME HEALTH CARE'); 
