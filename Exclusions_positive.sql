01b5efa6dc86b6638fbb7d4e7220b7fa3683efcc
Aggrus <gzalcman2015@my.fit.edu>
2018-07-15 12:07:51 +0300

    Better way of doing the same thing

--- Exclusions_positive.sql
+++ Exclusions_positive.sql
@@ -2,10 +2,8 @@
 SELECT admissions.subject_id, diagnoses_icd.icd9_code, PATIENTS.gender, admissions.admission_location, admissions.discharge_location 
 FROM ((admissions
 INNER JOIN PATIENTS  ON admissions.subject_id = PATIENTS.subject_id)
-INNER JOIN diagnoses_icd  ON admissions.subject_id = diagnoses_icd.subject_id);
+INNER JOIN diagnoses_icd  ON admissions.subject_id = diagnoses_icd.subject_id)
 --Checks that all information in the table conforms to the specifications
-SELECT admissions.subject_id, diagnoses_icd.icd9_code, PATIENTS.gender, admissions.admission_location, admissions.discharge_location 
-FROM admissions, patients, diagnoses_icd
 WHERE (diagnoses_icd.icd9_code IS NOT NULL) 
 AND (PATIENTS.gender = 'M' OR PATIENTS.gender = 'F') 
 AND (admissions.admission_location = 'TRANSFER FROM HOSP/EXTRAM' OR admissions.admission_location = 'EMERGENCY ROOM ADMIT' OR admissions.admission_location = 'PHYS REFERRAL/NORMAL DELI' OR admissions.admission_location = 'CLINIC REFERRAL/PREMATURE') 
