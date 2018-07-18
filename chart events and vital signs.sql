10c30632b8693130d534c792f0da09adb0c65417
Mallory Dixon <40456712+mallorydixon@users.noreply.github.com>
2018-07-18 12:15:36 +0300

    Add files via upload

--- chart events and vital signs.sql
+++ chart events and vital signs.sql
@@ -1,12 +1,32 @@
 SELECT chartevents.subject_id, chartevents.itemid, chartevents.value, chartevents.charttime, d_items.label, d_items.linksto,patients.dob, patients.gender, admissions.admittime
 FROM chartevents
-INNER JOIN patients ON charevents.subject_id = patients.subject_id
+INNER JOIN patients ON chartevents.subject_id = patients.subject_id
 INNER JOIN admissions ON chartevents.subject_id = admissions.subject_id
 INNER JOIN d_items ON chartevents.itemid = d_items.itemid
 
 WHERE chartevents.itemid IN (
     --candidate data elements for ML
-    '223770','223769','220210','219','615','618','227243','220047','223900','223901','223752','223751','225309','220179','227013','226755','223791','225813','198',
+    '223770' --O2 Saturation Pulse oximetry Alarm - LOW
+    ,'223769' --O2 Saturation Pulse oximetry Alarm - HIGH
+    ,'220210' --respiratory rate
+    ,'219' --high resp. rate
+    ,'615' --respiratory rate
+    ,'618' --resp. rate (total)
+    ,'227243' --Manual Blood Pressure Systolic Right
+    ,'220047' --Heart Rate Alarm - low
+    ,'223762' -- Temperature Celcius 
+    ,'220739' -- GCS - eye opening
+    ,'223900' -- GCS - motor response
+    ,'223901' -- GCS - verbal response
+    ,'223752' -- non-invasive blood pressure alarm - LOW
+    ,'223751' -- non-invasive blood pressure alarm - HIGH
+    ,'225309' -- ART BP Systolic
+    ,'220179' -- Noninvasive blood pressure systolic
+    ,'227013' -- GCSScore_ApacheIV
+    ,'226755' -- GCSApacheIIScore
+    ,'223791' -- Base Line Pain Level
+    ,'225813' -- Pain Level
+    ,'198', -- GCS Total
     --resp rate
     '615','618','220210','224690',
     --glucose
